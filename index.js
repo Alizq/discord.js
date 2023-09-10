e42c1d5b972d269118180a0236ff83195ad434c9
takejohn <105504345+takejohn@users.noreply.github.com>
2023-09-10 14:31:04 +0900

    インデントをスペース2個に統一

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const { Client, Events, Intents, Status, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({
-    intents: [
-		Intents.FLAGS.GUILDS,
-		Intents.FLAGS.GUILD_MESSAGES,
-		Intents.FLAGS.MESSAGE_CONTENT
-	]
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.MESSAGE_CONTENT
+  ]
 })
 
 
@@ -29,34 +29,34 @@ client.on("messageCreate", async msg => {
     const endTime = Date.now();
     const ping = endTime - startTime;
 
-	pingMessage.edit(`Pong! (${ping}ms)`);
+    pingMessage.edit(`Pong! (${ping}ms)`);
   }
 });
 
 function GenRandomNum() {
-	const min = 1;
-	const max = 5;
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  const min = 1;
+  const max = 5;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 client.on("messageCreate", async msg => {
   if (msg.content === "s!おみくじ") {
-	let number = GenRandomNum();
-	if (number == 1) {
-		msg.reply("吉")
-	}
-	if (number == 2) {
-		msg.reply("小吉")
-	}
-	if (number == 3) {
-		msg.reply("中吉")
-	}
-	if (number == 4) {
-		msg.reply("大吉")
-	}
-	if (number == 5) {
-		msg.reply("凶")
-	}
+    let number = GenRandomNum();
+    if (number == 1) {
+      msg.reply("吉")
+    }
+    if (number == 2) {
+      msg.reply("小吉")
+    }
+    if (number == 3) {
+      msg.reply("中吉")
+    }
+    if (number == 4) {
+      msg.reply("大吉")
+    }
+    if (number == 5) {
+      msg.reply("凶")
+    }
   }
 });
 
